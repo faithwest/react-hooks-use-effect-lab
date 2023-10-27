26218fbafdf2c01578a8eb89c0f36c2473d2cd9c
faithwest <faithchepkoech425@gmail.com>
2023-10-28 00:35:01 +0300

    initialized project

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    // Timer Helper: Runs every second
+    const timer = setTimeout(() => {
+      if (timeRemaining > 0) {
+        // Takes away 1 second from the clock
+        setTimeRemaining(timeRemaining - 1);
+      } else {
+        // Time's up! Call onAnswered with 'false'
+        onAnswered(false);
+      }
+    }, 1000); // 1000 milliseconds = 1 second
+
+    return () => {
+      // Clean up: Stop the timer when unmounting
+      clearTimeout(timer);
+    };
+  }, [timeRemaining, onAnswered]);
 
   function handleAnswer(isCorrect) {
+    // Restart the timer by setting it back to 10
     setTimeRemaining(10);
+    // If the answer is correct, we're happy; if not, we say "Oops!"
     onAnswered(isCorrect);
   }
 
